cc564c877e7c41162e0ea35b3b8021be5efdadc4
Basil Papadimas <papadimas@protonmail.com>
2024-05-23 18:42:43 +0300

    faster

--- test-res.sh
+++ test-res.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#!/bin/sh
 #the command to runn the code
-gcc -o output p3220120-p3220150-p3220227-pizza.c -pthread
-./output 100 10
+gcc -O3 -march=native -mtune=native -pthread -o ex p3220120-p3220150-p3220227-pizza.c -pthread
+./ex 100 10
